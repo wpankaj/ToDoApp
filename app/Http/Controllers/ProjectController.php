0b0ee0f57b5c6e44556758a37b8272c17c209e9d
Pankaj Wadhwa <w.pankaj@gmail.com>
2016-08-21 23:03:53 +0530

    Adding a Store function to Project Controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Str;
+
+
+use App\Http\Controllers\Controller;
+use App\Project;
 use App\Http\Requests;
 use Carbon\Carbon;
-use Auth;
+use App\Http\Requests\ProjectRequest;
 
 class ProjectController extends Controller
 {
@@ -19,6 +26,41 @@ class ProjectController extends Controller
     $today = Carbon::now()->formatlocalized("%a %d %b %y");
     return view("pages.projects.home", compact("projects", "today", "currenttime", "user"));
 
+  }
+
+  public function store(ProjectRequest $request)
+  {
+    if($request->ajax())
+    {
+      $slug = Str::slug($request->name);
+
+      Auth::user()->projects()->create(
+          Array(
+            "name"=>$request->name,
+            "slug"=>$slug,
+            "desc"=>$request->desc,
+            "duedate"=>$request->duedate
+            )
+      );
+
+      $response = Array("msg"=>"Project Saved. Please close modal");
+      return Response::json($response);
+    }
+    else
+    {
+      $slug = Str::slug($request->name);
+
+      Auth::user()->projects()->create(
+          Array(
+            "name"=>$request->name,
+            "slug"=>$slug,
+            "desc"=>$request->desc,
+            "duedate"=>$request->duedate
+            )
+      );
+
+      redirect("projects")->with("success", "Project ".ucwords($request->name). " has been created");
+    }
   }
     //
 }
